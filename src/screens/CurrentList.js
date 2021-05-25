221e99e990f1f05da203eae6b24b084f77c247cd
nllew <n.lewis.perc@gmail.com>
2021-05-25 09:22:49 -0500

    additem completed

--- src/screens/CurrentList.js
+++ src/screens/CurrentList.js
@@ -2,10 +2,12 @@ import React from "react";
 import { View, Text, SafeAreaView, ScrollView, FlatList } from "react-native";
 import nachos from "../data/nachos";
 import ListItem, { Separator } from "../components/ListItem";
+import AddItem from "../components/AddItem";
 
 export default () => {
     return (
         <SafeAreaView>
+            <AddItem />
             <FlatList
                 data={nachos}
                 renderItem={({ item, index }) => (
@@ -20,24 +22,4 @@ export default () => {
             />
         </SafeAreaView>
     );
-
-    // return (
-    //     <SafeAreaView>
-    //         <ScrollView>
-    //             {nachos.map((item, index) => (
-    //                 <React.Fragment>
-    //                     <Separator />
-    //                     <ListItem
-    //                         key={item.id}
-    //                         name={item.name}
-    //                         onFavoritePress={() =>
-    //                             alert("todo: handle favorite!")
-    //                         }
-    //                         isFavorite={index < 2}
-    //                     />
-    //                 </React.Fragment>
-    //             ))}
-    //         </ScrollView>
-    //     </SafeAreaView>
-    // );
 };
