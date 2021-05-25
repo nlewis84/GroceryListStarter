9dae43b4e20cda3142df5c61c63c4cd7108dc736
nllew <n.lewis.perc@gmail.com>
2021-05-25 09:28:04 -0500

    moved addItem to listHeader

--- src/screens/CurrentList.js
+++ src/screens/CurrentList.js
@@ -1,25 +1,29 @@
 import React from "react";
-import { View, Text, SafeAreaView, ScrollView, FlatList } from "react-native";
+import { SafeAreaView, FlatList, KeyboardAvoidingView } from "react-native";
 import nachos from "../data/nachos";
 import ListItem, { Separator } from "../components/ListItem";
 import AddItem from "../components/AddItem";
 
 export default () => {
     return (
-        <SafeAreaView>
-            <AddItem />
-            <FlatList
-                data={nachos}
-                renderItem={({ item, index }) => (
-                    <ListItem
-                        name={item.name}
-                        onFavoritePress={() => alert("todo: handle favorite!")}
-                        isFavorite={index < 2}
-                    />
-                )}
-                keyExtractor={(item) => item.id}
-                ItemSeparatorComponent={() => <Separator />}
-            />
+        <SafeAreaView style={{ flex: 1 }}>
+            <KeyboardAvoidingView style={{ flex: 1 }} behavior="padding">
+                <FlatList
+                    data={nachos}
+                    renderItem={({ item, index }) => (
+                        <ListItem
+                            name={item.name}
+                            onFavoritePress={() =>
+                                alert("todo: handle favorite!")
+                            }
+                            isFavorite={index < 2}
+                        />
+                    )}
+                    keyExtractor={(item) => item.id}
+                    ItemSeparatorComponent={() => <Separator />}
+                    ListHeaderComponent={() => <AddItem />}
+                />
+            </KeyboardAvoidingView>
         </SafeAreaView>
     );
 };
